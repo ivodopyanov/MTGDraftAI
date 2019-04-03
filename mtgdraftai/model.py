4821b2c6ec43cd9606e5b8631bff4301e903b94d
Igor Vodopyanov <ivodopyanov@naumen.ru>
2019-04-03 11:36:09 +0500

    Dataset & train code

---
+++ mtgdraftai/model.py
@@ -0,0 +1,146 @@
+import tensorflow as tf
+import numpy as np
+from math import pi, sqrt
+
+
+EPS = 1e-12
+
+class Model(object):
+    def __init__(self, sess, config):
+        self.sess = sess
+        self.config = config
+
+    def build_trainer(self):
+        packs = tf.placeholder(tf.int32, shape=[None, None], name="packs")
+        picks = tf.placeholder(tf.int32, shape=[None, None], name="picks")
+
+
+
+        dropout = tf.placeholder(dtype=tf.float32, shape=[], name="dropout")
+        lr = tf.placeholder(dtype=tf.float32, shape=[], name="lr")
+
+        total_cards = tf.concat([packs, picks], axis=-1)
+        final_embeddings = self.transformer(total_cards, dropout)
+        final_embeddings = final_embeddings[:,:tf.shape(packs)[1]]
+        total_scores = tf.layers.Dense(1, name="q")(final_embeddings)
+        total_scores = tf.reduce_sum(total_scores, axis=-1)
+        total_scores = tf.where(tf.equal(packs, 0), tf.ones_like(total_scores)*(-1e10), total_scores)
+
+        chosen_cards = tf.cast(tf.argmax(total_scores, axis=-1), tf.int32, name="chosen_cards")
+        scores_pred = tf.identity(total_scores, name="scores_pred")
+
+        this_pick = tf.placeholder(tf.int32, shape=[None], name="choice")
+        loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=total_scores,labels=this_pick)
+        loss = tf.reduce_mean(loss, name="loss")
+
+
+        optimizer = tf.train.AdamOptimizer(lr)
+        self.gradients = optimizer.compute_gradients(loss)
+        train_op = optimizer.apply_gradients(self.gradients, name="train_op")
+        #self.check = tf.add_check_numerics_ops()
+
+    def transformer(self, total_cards, dropout):
+
+        mask = tf.cast(tf.equal(total_cards, 0), dtype=tf.float32)
+        embeddings_var = tf.get_variable(name="embeddings_var",
+                                               dtype=tf.float32,
+                                               initializer=tf.initializers.random_uniform(minval=-0.1, maxval=0.1),
+                                               shape=[self.config['cards_count'], self.config['emb_dim']])
+        inputs = tf.nn.embedding_lookup(embeddings_var, total_cards, name="embeddings")
+        x = inputs
+        for i in range(self.config['transformer_layers_num']):
+            with tf.variable_scope("layer_{}".format(i)):
+                with tf.variable_scope("self_attention"):
+                    delta = tf.contrib.layers.layer_norm(x)
+                    delta = self.attention(delta, delta, mask, dropout)
+                    delta = tf.nn.dropout(delta, dropout)
+                    x = x + delta
+                with tf.variable_scope("feed_forward"):
+                    delta = tf.contrib.layers.layer_norm(x)
+                    delta = self.feed_forward(delta, dropout)
+                    delta = tf.nn.dropout(delta, dropout)
+                    x = x + delta
+        with tf.variable_scope("output_norm"):
+            x = tf.contrib.layers.layer_norm(x)
+        return x
+
+
+    def attention(self, query, memory, mask, dropout):
+        depth = self.config['emb_dim'] // self.config['transformer_num_heads']
+
+        q = tf.layers.Dense(self.config['emb_dim'], name="q")(query)
+        k = tf.layers.Dense(self.config['emb_dim'], name="k")(memory)
+        v = tf.layers.Dense(self.config['emb_dim'], name="v")(memory)
+
+        q = tf.reshape(q, [tf.shape(q)[0], tf.shape(q)[1], self.config['transformer_num_heads'], depth])
+        k = tf.reshape(k, [tf.shape(k)[0], tf.shape(k)[1], self.config['transformer_num_heads'], depth])
+        v = tf.reshape(v, [tf.shape(v)[0], tf.shape(v)[1], self.config['transformer_num_heads'], depth])
+
+        q = tf.transpose(q, [0, 2, 1, 3])
+        k = tf.transpose(k, [0, 2, 1, 3])
+        v = tf.transpose(v, [0, 2, 1, 3])
+
+        logits = tf.matmul(q, k, transpose_b=True)
+
+        logits = logits * (depth ** -0.5)
+
+        e_logits = tf.exp(logits - tf.reduce_max(logits, axis=-1, keepdims=True))
+        e_logits = e_logits * tf.expand_dims(tf.expand_dims(mask, axis=1), axis=1)
+        att_weights = e_logits / (tf.reduce_sum(e_logits, axis=-1, keepdims=True)+EPS)
+        att_weights = tf.nn.dropout(att_weights, dropout)
+
+        attention_output = tf.matmul(att_weights, v)
+        attention_output = tf.transpose(attention_output, [0, 2, 1, 3])
+        attention_output = tf.reshape(attention_output, [tf.shape(attention_output)[0], tf.shape(attention_output)[1], self.config['emb_dim']])
+
+        attention_output = tf.layers.Dense(self.config['emb_dim'], name="output_transform")(attention_output)
+        return attention_output
+
+    def feed_forward(self, x, dropout):
+        x = tf.layers.Dense(self.config['transformer_filter_size'], name="filter_layer")(x)
+        x = 0.5*x*(1+tf.tanh(sqrt(2/pi)*(x+0.044715*tf.pow(x, 3))))
+        x = tf.nn.dropout(x, dropout)
+        x = tf.layers.Dense(self.config['emb_dim'], name="output_layer")(x)
+        return x
+
+
+    def build(self):
+        self.build_trainer()
+
+    def get_weights(self):
+        return self.sess.run(self.sess.graph._collections['variables'])
+
+    def set_weights(self, weights):
+        ops = []
+        feed = {}
+        for idx, variable in enumerate(self.sess.graph._collections['variables']):
+            ops.append(variable._initializer_op)
+            feed[variable._initializer_op.inputs[1]] = weights[idx]
+        self.sess.run(ops, feed_dict=feed)
+
+    def train(self, packs, picks, choice, dropout, lr):
+        feed_dict = {self.sess.graph.get_tensor_by_name("packs:0"): packs,
+                     self.sess.graph.get_tensor_by_name("picks:0"): picks,
+                     self.sess.graph.get_tensor_by_name("choice:0"): choice,
+                     self.sess.graph.get_tensor_by_name("dropout:0"): dropout,
+                     self.sess.graph.get_tensor_by_name("lr:0"): lr}
+
+        _, train_loss, scores, choice_pred = self.sess.run([self.sess.graph.get_operation_by_name("train_op"),
+                                                            self.sess.graph.get_tensor_by_name("loss:0"),
+                                                            self.sess.graph.get_tensor_by_name("scores_pred:0"),
+                                                            self.sess.graph.get_tensor_by_name("chosen_cards:0")],
+                                                           feed_dict=feed_dict)
+        accuracy = np.average(choice==choice_pred)
+        return train_loss, accuracy
+
+    def evaluate(self, packs, picks, choice):
+        feed_dict = {self.sess.graph.get_tensor_by_name("packs:0"): packs,
+                     self.sess.graph.get_tensor_by_name("picks:0"): picks,
+                     self.sess.graph.get_tensor_by_name("choice:0"): choice,
+                     self.sess.graph.get_tensor_by_name("dropout:0"): 1.0}
+        test_loss, choice_pred = self.sess.run([self.sess.graph.get_tensor_by_name("loss:0"),
+                                                self.sess.graph.get_tensor_by_name("chosen_cards:0")],
+                                               feed_dict=feed_dict)
+        accuracy = np.average(choice==choice_pred)
+
+        return test_loss, accuracy
---
+++ mtgdraftai/train.py
@@ -0,0 +1,117 @@
+import os
+import sys
+import json
+import numpy as np
+from random import random
+import tensorflow as tf
+from mtgdraftai.model import Model
+
+def train():
+    homedir = os.path.dirname(os.path.abspath(__file__))
+    with open(os.path.join(homedir, "train_config.json"), "rt") as f:
+        train_config = json.load(f)
+    train_data, test_data, card_vocab_encode, card_vocab_decode = load_data(train_config)
+    with open(os.path.join(homedir, "model_config.json"), "rt") as f:
+        model_config = json.load(f)
+    model_config['cards_count'] = len(card_vocab_decode)
+    if not os.path.exists(train_config['output_dir']):
+        os.makedirs(train_config['output_dir'])
+    with open(os.path.join(train_config['output_dir'], "model_config.json"), "wt") as f:
+        json.dump(model_config, f)
+    with open(os.path.join(train_config['output_dir'], "model_vocab.json"), "wt") as f:
+        json.dump(card_vocab_decode, f)
+    sys.stdout.write("\nEncoding train data\n")
+    train_data = encode_data(train_data, card_vocab_encode)
+    sys.stdout.write("\nEncoding test data\n")
+    test_data = encode_data(test_data, card_vocab_encode)
+    with tf.Session() as sess:
+        model = Model(sess, model_config)
+        model.build()
+        sess.run(tf.global_variables_initializer())
+        run_train(model, train_data, test_data, train_config)
+
+
+def load_data(train_config):
+    train_data = []
+    test_data = []
+    card_vocab = set()
+    draft_filenames = os.listdir(train_config['source_dir'])
+    sys.stdout.write("Loading data\n")
+    for draft_id, draft_filename in enumerate(draft_filenames):
+        if draft_id%100 == 0:
+            sys.stdout.write("\r{} / {} : {}       ".format(draft_id, len(draft_filenames), draft_filename))
+        with open(os.path.join(train_config['source_dir'], draft_filename), "rt") as f:
+            draft_json = json.load(f)
+            for round in draft_json['packs']:
+                for booster in round:
+                    card_vocab.update(booster)
+            picked = np.zeros((len(draft_json['player_names']), 3*train_config['pack_size']), dtype=np.int32)
+            for pack_num, booster in enumerate(draft_json['boosters']):
+                for pick_num in range(train_config['pack_size']):
+                    for player_pos, bot in enumerate(draft_json['bots']):
+                        if pack_num % 2 == 0:
+                            original_pack_pos = (player_pos-pick_num)%(len(draft_json['player_names']))
+                        else:
+                            original_pack_pos = (player_pos+pick_num)%(len(draft_json['player_names']))
+                        pick_pos = draft_json['picks'][pack_num][player_pos][pick_num] - 1
+                        if not bot and len([card_id for card_id in draft_json['packs'][pack_num][original_pack_pos] if card_id!=0]) > 5 and draft_json['packs'][pack_num][original_pack_pos][pick_pos]!=0:
+                            sample = dict(pack=draft_json['packs'][pack_num][original_pack_pos][:],
+                                          picks=picked[player_pos].tolist(),
+                                          choice=pick_pos)
+                            if random() < train_config['train_test_split']:
+                                train_data.append(sample)
+                            else:
+                                test_data.append(sample)
+                        picked[player_pos][pack_num*train_config['pack_size']+pick_num] = draft_json['packs'][pack_num][original_pack_pos][pick_pos]
+                        draft_json['packs'][pack_num][original_pack_pos][pick_pos] = 0
+
+    card_vocab_decode = [0]+sorted(card_vocab)
+    card_vocab_encode = dict()
+    for pos, id in enumerate(card_vocab_decode):
+        card_vocab_encode[id] = pos
+    return train_data, test_data, card_vocab_encode, card_vocab_decode
+
+
+def encode_data(data, card_vocab_encode):
+    for sample_id, sample in enumerate(data):
+        if sample_id%100 == 0:
+            sys.stdout.write("\r{} / {}     ".format(sample_id, len(data)))
+        sample['picks'] = [card_vocab_encode[card] for card in sample['picks']]
+        sample['pack'] = [card_vocab_encode[card] for card in sample['pack']]
+    return data
+
+
+def run_train(model, train_data, test_data, train_config):
+    with tf.Session() as sess:
+        saver = tf.train.Saver(sess.graph._collections['variables'])
+        best_acc = 0
+        for epoch_num in range(999):
+            sys.stdout.write("\nEpoch {}\nTraining\n".format(epoch_num))
+            losses = []
+            for batch_num in range(len(train_data) // train_config['batch_size']):
+                picks = [sample['picks'] for sample in train_data[train_config['batch_size']*batch_num:train_config['batch_size']*(batch_num+1)]]
+                packs = [sample['pack'] for sample in train_data[train_config['batch_size']*batch_num:train_config['batch_size']*(batch_num+1)]]
+                choice = [sample['choice'] for sample in train_data[train_config['batch_size']*batch_num:train_config['batch_size']*(batch_num+1)]]
+                loss = model.train(packs, picks, choice, train_config['dropout'], train_config['lr'])
+                losses.append(loss)
+                if batch_num%10 == 0:
+                    sys.stdout.write("\r{}/{} loss={:.4f}     ".format(batch_num*train_config['batch_size'], len(train_data), np.mean(losses)))
+            sys.stdout.write("\nTesting\n")
+            losses = []
+            accs = []
+            for batch_num in range(len(test_data) // train_config['batch_size']):
+                picks = [sample['picks'] for sample in test_data[train_config['batch_size']*batch_num:train_config['batch_size']*(batch_num+1)]]
+                packs = [sample['pack'] for sample in test_data[train_config['batch_size']*batch_num:train_config['batch_size']*(batch_num+1)]]
+                choice = [sample['choice'] for sample in test_data[train_config['batch_size']*batch_num:train_config['batch_size']*(batch_num+1)]]
+                loss, acc = model.evaluate(packs, picks, choice)
+                losses.append(loss)
+                accs.append(acc)
+                if batch_num%10 == 0:
+                    sys.stdout.write("\r{}/{} loss={:.4f} acc={:.4f}     ".format(batch_num*train_config['batch_size'], len(test_data), np.mean(losses), np.mean(accs)))
+            if np.mean(accs) > best_acc:
+                best_acc = np.mean(accs)
+                sys.stdout.write("\nSaving best model acc = {:.4f}\n".format(best_acc))
+                saver.save(sess, os.path.join(train_config['output_dir'],"transformer_model"))
+
+if __name__ == "__main__":
+    train()
